fbcbcb53ce6af822b5a476ae27a1a32dc59db246
Mike Bruce <mike@hypalynx.com>
2023-02-06 06:27:57 +0000

    deal with multi-chunk/large messages

---
+++ lua/replica/client.lua
@@ -0,0 +1,76 @@
+local bencode = require("replica.bencode")
+
+local decode, encode = bencode.decode, bencode.encode
+local insert, concat = table.insert, table.concat
+
+local module = {}
+
+-- Learning notes: if you have a uv__check_before_write error, make sure you aren't trying to sent tables over the
+-- wire. This can happen if you aren't bencoding messages first.
+
+local tcp_client -- TCP client for sending messages
+local session_id
+local id = "replica.client"
+local partial_chunks = nil
+
+-- TODO should be an internal function, exposed for testing
+module.clone = function()
+  tcp_client:write(encode({op="clone"}))
+end
+
+-- internal function for reading messages that come back from the nREPL.
+read = function(chunk)
+  local message, _
+
+  if partial_chunks == nil then
+    message, _ = decode(chunk)
+  else
+    message, _ = decode(concat(partial_chunks) .. chunk)
+  end
+  print(vim.inspect(message))
+
+  if message == nil then
+    -- in this case, the chunk is not parseable which probably means we have part of a big message.
+    if partial_chunks == nil then
+      partial_chunks = {}
+    end
+    insert(partial_chunks, chunk)
+  else
+    partial_chunks = {} -- reset as we have a full parsed message
+    if message["new-session"] ~= nil then
+      session_id = message["new-session"]
+      print("new session id: "..session_id)
+    end
+  end
+end
+
+-- TODO find port based on .nrepl-port?
+-- TODO although this is async.. if you do not do anything it takes ages to return the message?
+-- I guess this is the nature of async? but it seems to immediately return if I move the cursor.
+module.connect = function(host, port)
+  local uv = vim.loop
+  tcp_client = uv.new_tcp()
+  local connection = uv.tcp_connect(tcp_client, host, port, function(err)
+    assert(not err, err)
+  end)
+
+  module.clone()
+  tcp_client:read_start(function(err, chunk)
+    assert(not err, err)
+    -- TODO is this actually a chunk? what happens if a large message is sent to us?
+    if chunk then
+      read(chunk)
+    else
+      client:shutdown()
+      client:close()
+    end
+  end)
+
+  uv.run()
+end
+
+module.eval = function(code)
+  tcp_client:write(encode({id=id, op="eval", code=code, session=session_id}))
+end
+
+return module
--- lua/replica/commands.lua
+++ lua/replica/commands.lua
@@ -1,4 +1,4 @@
-local bencode = require("replica.bencode")
+local client = require("replica.client")
 
 local module = {}
 
@@ -13,79 +13,18 @@ local module = {}
 -- https://neovim.io/doc/user/luvref.html#luv-intro
 -- lua print(vim.inspect(vim.loop))
 
-module.example = function()
-  local uv = vim.loop
-  local server = uv.new_tcp()
-  server:bind("127.0.0.1", 52200)
-  server:listen(128, function (err)
-    assert(not err, err)
-    local client = uv.new_tcp()
-    server:accept(client)
-    client:read_start(function (err, chunk)
-      assert(not err, err)
-      if chunk then
-        print("got message back")
-        print(chunk)
-      else
-        client:shutdown()
-        client:close()
-      end
-    end)
-
-    -- client:write("d2:op5:clonee")
-  end)
-  print("TCP server started with initial clone message")
-  uv.run()
-end
-
 -- TODO having a module.client won't work for multiple nREPL connections, maybe we will need a collection of
 -- connections for clj and cljs to connect at the same time?
 -- XXX answer: I think we have a single TCP connection and grab 2 sessions?
-module.open_example = function()
-  local uv = vim.loop
-  -- local client = uv.new_tcp()
-  module.client = uv.new_tcp()
-  local connection = uv.tcp_connect(module.client, "127.0.0.1", 52216, function (err)
-    assert(not err, err)
-  end)
-
-  module.client:write(bencode.encode({op="clone"}))
-  module.client:read_start(function (err, chunk)
-    assert(not err, err)
-
-    if chunk then
-      print("got message back")
-      print(chunk)
-      -- this will always try to get a session id
-      -- assumes session-id will always be 36 characters long
-      module.id = vim.split(chunk, ":")[3]:sub(0, 36)
-      print(module.id)
-    else
-      module.client:shutdown()
-      module.client:close()
-    end
-  end)
-end
-
-module.message = function(id, session, code)
-  local msg = string.format("d4:code%s:%s2:id%s:%s2:op4:eval7:session%s:%se", string.len(code), code, string.len(id), id, string.len(session), session)
-  print(msg)
-  module.client:write(msg)
-end
-
-module.clone = function()
-  module.client:write("d2:op5:clonee")
-end
 
 module.setup = function()
   -- local cmd = nvim_create_user_command
 
   -- cmd("Connect" 'echo "Hello world!"', {})
   -- vim.api.nvim_create_user_command("Connect", 'echo "Hello world!"', {})
-  vim.api.nvim_create_user_command("Connect", module.open_example, {})
-  vim.api.nvim_create_user_command("Clone", module.clone, {})
-  vim.api.nvim_create_user_command("Message", function () module.message("test", module.id, "(+ 40 2)") end, {})
-  return
+  vim.api.nvim_create_user_command("Connect", function() client.connect("127.0.0.1", 54038) end, {})
+  vim.api.nvim_create_user_command("Clone", client.clone, {})
+  vim.api.nvim_create_user_command("TestMessage", function () client.eval("(+ 40 2)") end, {})
 end
 
 return module
