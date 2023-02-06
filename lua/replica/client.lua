a72ab846eb78a759cbf2448f4ae18a17bcf733d2
Mike Bruce <mike@hypalynx.com>
2023-02-06 07:08:55 +0000

    better separation of debug logs vs message output

--- lua/replica/client.lua
+++ lua/replica/client.lua
@@ -3,15 +3,32 @@ local bencode = require("replica.bencode")
 local decode, encode = bencode.decode, bencode.encode
 local insert, concat = table.insert, table.concat
 
+-- TODO allow this to be controlled via config
+local debug = false
+local debug_eval_only = true -- only print full message for the eval session
+
 local module = {}
+
+-- nREPL reference, including best practices: https://nrepl.org/nrepl/building_clients.html TODO keep evals in one
+-- session and tooling-related evaluations (e.g for code completion) in another to avoid *1 and *e from being messed
+-- up.
 
 -- Learning notes: if you have a uv__check_before_write error, make sure you aren't trying to sent tables over the
 -- wire. This can happen if you aren't bencoding messages first.
 
 local tcp_client -- TCP client for sending messages
-local session_id
+local main_session_id
+local eval_session_id
 local id = "replica.client"
 local partial_chunks = nil
+
+local log = function(message)
+    local log_file_path = './replica.log'
+    local log_file = io.open(log_file_path, "a")
+    io.output(log_file)
+    io.write(message.."\n")
+    io.close(log_file)
+end
 
 clone = function()
   tcp_client:write(encode({op="clone"}))
@@ -26,7 +43,9 @@ read = function(chunk)
   else
     message, _ = decode(concat(partial_chunks) .. chunk)
   end
-  print(vim.inspect(message))
+  if debug then
+    log(vim.inspect(message))
+  end
 
   if message == nil then
     -- in this case, the chunk is not parseable which probably means we have part of a big message.
@@ -37,8 +56,26 @@ read = function(chunk)
   else
     partial_chunks = {} -- reset as we have a full parsed message
     if message["new-session"] ~= nil then
-      session_id = message["new-session"]
-      print("new session id: "..session_id)
+      if eval_session_id == nil then
+        eval_session_id = message["new-session"]
+        if debug then
+          log("new eval session id: "..eval_session_id)
+        end
+      else
+        main_session_id = message["new-session"]
+        if debug then
+          log("new main session id: "..main_session_id)
+        end
+      end
+    end
+
+    if message["session"] == eval_session_id then
+      if debug_eval_only then
+        log(vim.inspect(message))
+      end
+      if message["value"] ~= nil then
+        print(message["value"])
+      end
     end
   end
 end
@@ -53,22 +90,23 @@ module.connect = function(host, port)
     assert(not err, err)
   end)
 
-  clone()
+  clone() -- for a seperate eval session id
+  clone() -- for a new main session id, do this last so it is default for messages we did not initiate
   tcp_client:read_start(function(err, chunk)
     assert(not err, err)
     if chunk then
       read(chunk)
     else
-      client:shutdown()
-      client:close()
+      tcp_client:shutdown()
+      tcp_client:close()
     end
   end)
 
   uv.run()
 end
 
-module.eval = function(ns, code)
-  tcp_client:write(encode({id=id, ns=ns, op="eval", code=code, session=session_id}))
+module.eval = function(code)
+  tcp_client:write(encode({id=id, op="eval", code=code, session=eval_session_id}))
 end
 
 return module
--- lua/replica/commands.lua
+++ lua/replica/commands.lua
@@ -2,9 +2,21 @@ local client = require("replica.client")
 
 local module = {}
 
+module.eval = function(args)
+  if args["args"] ~= "" then
+    client.eval(args["args"])
+  end
+end
+
+module.test_args = function(args)
+  print(vim.inspect(args))
+end
+
 module.setup = function()
   vim.api.nvim_create_user_command("Connect", function() client.connect("127.0.0.1", 54038) end, {})
-  vim.api.nvim_create_user_command("TestMessage", function () client.eval("user", "(+ 40 2)") end, {})
+  vim.api.nvim_create_user_command("Eval", module.eval, { nargs='?' })
+  vim.api.nvim_create_user_command("TestArgs", module.test_args, { nargs='?' })
+  vim.api.nvim_create_user_command("TestMessage", function () client.eval("(+ 40 2)") end, {})
 end
 
 return module
